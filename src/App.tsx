4df2e189765f7b39c3be9f1327930af21e32c01f
Sara <sarailic431@gmail.com>
2025-09-23 20:53:39 +0200

    starting application

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import words from "./wordList.json";
 
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [huessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  console.log(wordToGuess);
   return (
     <>
       <h1 className="text-red-400">Hangman Word Game</h1>
