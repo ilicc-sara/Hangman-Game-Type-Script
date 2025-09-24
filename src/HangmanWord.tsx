182687eb599eb80cbcd193533c9200d365f35c0a
Sara <sarailic431@gmail.com>
2025-09-24 17:49:25 +0200

    guessed letters

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -2,11 +2,21 @@ import React from "react";
 
 function HangmanWord() {
   const word = "test";
+  const guessedLetters = ["t", "e"];
   return (
     <div className="flex gap-[.25em] text-[6rem] font-bold uppercase font-mono">
       {word.split("").map((letter, index) => (
-        <span className="border-b-[0.1em] border-b-solid border-b-black">
-          <span className="invisible">{letter}</span>
+        <span
+          key={index}
+          className="border-b-[0.1em] border-b-solid border-b-black"
+        >
+          <span
+            className={`${
+              guessedLetters.includes(letter) ? "visible" : "invisible"
+            }`}
+          >
+            {letter}
+          </span>
         </span>
       ))}
     </div>
